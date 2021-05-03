f15ab6b349ca0dfa9ca0a284a17649a22252a3f6
hzndr <hania.zander@gmail.com>
2021-05-03 16:47:19 +0200

    json server changed

--- src/App.js
+++ src/App.js
@@ -22,7 +22,9 @@ function App() {
 
   //Fetch tasks
   const fetchTasks = async () => {
-    const res = await fetch("http://localhost:5000/tasks");
+    const res = await fetch(
+      "https://my-json-server.typicode.com/hzndr/json-server/tasks"
+    );
     const data = await res.json();
 
     return data;
@@ -30,7 +32,9 @@ function App() {
 
   //Fetch task
   const fetchTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const res = await fetch(
+      `https://my-json-server.typicode.com/hzndr/json-server/tasks/${id}`
+    );
     const data = await res.json();
 
     return data;
@@ -38,13 +42,16 @@ function App() {
 
   //Add Task
   const addTask = async (task) => {
-    const res = await fetch("http://localhost:5000/tasks", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(task),
-    });
+    const res = await fetch(
+      "https://my-json-server.typicode.com/hzndr/json-server/tasks",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(task),
+      }
+    );
 
     const data = await res.json();
     setTasks([...tasks, data]);
@@ -55,9 +62,12 @@ function App() {
 
   //Delete Task
   const deleteTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: "DELETE",
-    });
+    const res = await fetch(
+      `https://my-json-server.typicode.com/hzndr/json-server/tasks/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
     res.status === 200
       ? setTasks(tasks.filter((task) => task.id !== id))
       : alert("Error Deleting This Task");
@@ -68,13 +78,16 @@ function App() {
     const taskToToggle = await fetchTask(id);
     const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
 
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(updTask),
-    });
+    const res = await fetch(
+      `https://my-json-server.typicode.com/hzndr/json-server/tasks/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(updTask),
+      }
+    );
 
     const data = await res.json();
 
